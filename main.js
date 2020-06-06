65cef029f2a555796ecc75f2a72e85c1da9af341
Pebanh <peter.banh95@gmail.com>
2020-06-06 02:05:34 -0400

    Add logic for eye movement

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 paper.install(window);
 // Keep global references to both tools, so the HTML
 // links below can access them.
-var tool1;
-var tool2;
-var tool3;
-var tool4;
+var bodyDraw;
+var eye1;
+var eye2;
+var mouth;
 
 window.onload = function () {
   paper.setup("myCanvas");
@@ -18,20 +18,19 @@ window.onload = function () {
     path.add(event.point);
   }
 
-  tool1 = new Tool();
-  tool1.onMouseDown = onMouseDown;
+  bodyDraw = new Tool();
+  bodyDraw.onMouseDown = onMouseDown;
 
-  tool1.onMouseDrag = function (event) {
+  bodyDraw.onMouseDrag = function (event) {
     path.add(event.point);
   };
 
   let addSubtract = true;
 
-  tool1.onMouseUp = function (event) {
+  bodyDraw.onMouseUp = function (event) {
     path.closed = true;
     copy = path.clone();
     copy.strokeColor = new Color(255, 255, 255, 0);
-    console.log(path.segments.index);
     path.fillColor = "#ff0000";
     path.strokeColor = "#ff0000";
     view.onFrame = function (event) {
@@ -51,18 +50,33 @@ window.onload = function () {
         i++;
       } while (!path.segments[i].isLast());
     };
-    tool2.activate();
+    eye1.activate();
   };
 
-  tool2 = new Tool();
-  tool2.minDistance = 20;
-  tool2.onMouseUp = function (event) {
+  eye1 = new Tool();
+  eye1.minDistance = 20;
+
+  var myCircle;
+  var myCircle2;
+
+  let clientXPosition;
+  let clientYPosition;
+
+  let xVector;
+  let yVector;
+  let vectorLength;
+
+  let pupilXDist;
+  let pupilYDist;
+  let pupilDist;
+
+  eye1.onMouseUp = function (event) {
     if (path.contains(event.point)) {
-      var myCircle = new Path.Circle({
+      myCircle = new Path.Circle({
         center: event.point,
         radius: 20,
       });
-      var myCircle2 = new Path.Circle({
+      myCircle2 = new Path.Circle({
         center: event.point,
         radius: 10,
       });
@@ -70,33 +84,151 @@ window.onload = function () {
       myCircle.fillColor = "white";
       myCircle2.strokeColor = "black";
       myCircle2.fillColor = "black";
-      tool3.activate();
+
+      view.onMouseMove = function (event) {
+        clientXPosition = event.point.x;
+        clientYPosition = event.point.y;
+      };
+
+      view.onFrame = function (event) {
+        if (!isNaN(clientXPosition) && !isNaN(clientYPosition)) {
+          xVector = clientXPosition - myCircle.position.x;
+          yVector = clientYPosition - myCircle.position.y;
+          vectorLength = Math.sqrt(xVector * xVector + yVector * yVector);
+          xVector /= vectorLength;
+          yVector /= vectorLength;
+
+          pupilXDist = myCircle2.position.x - myCircle.position.x + xVector;
+          pupilYDist = myCircle2.position.y - myCircle.position.y + yVector;
+          pupilDist = Math.sqrt(
+            pupilXDist * pupilXDist + pupilYDist * pupilYDist
+          );
+
+          if (pupilDist < 10) {
+            myCircle2.position.x += xVector;
+            myCircle2.position.y += yVector;
+          }
+          // Each frame, change the fill color of the path slightly by
+          // adding 1 to its hue:
+          path.fillColor.hue += 1;
+          let i = 0;
+          let randomValue;
+          addSubtract = !addSubtract;
+          do {
+            var sinus = Math.sin(event.time * 3 + i) * 3;
+            randomValue = Math.random() * 1;
+            var segment = path.segments[i];
+            var refSegment = copy.segments[i];
+            segment.point.y = refSegment.point.y + sinus;
+            segment.point.x = refSegment.point.x + sinus;
+            i++;
+          } while (!path.segments[i].isLast());
+        }
+      };
+
+      eye2.activate();
     }
   };
 
-  tool3 = new Tool();
-  tool3.minDistance = 20;
-  tool3.onMouseUp = function (event) {
+  eye2 = new Tool();
+  eye2.minDistance = 20;
+
+  var myCircle3;
+  var myCircle4;
+
+  let pupilXDist2;
+  let pupilYDist2;
+  let pupilDist2;
+
+  eye2.onMouseUp = function (event) {
     if (path.contains(event.point)) {
-      var myCircle = new Path.Circle({
+      myCircle3 = new Path.Circle({
         center: event.point,
         radius: 20,
       });
-      var myCircle2 = new Path.Circle({
+      myCircle4 = new Path.Circle({
         center: event.point,
         radius: 10,
       });
-      myCircle.strokeColor = "black";
-      myCircle.fillColor = "white";
-      myCircle2.strokeColor = "black";
-      myCircle2.fillColor = "black";
-      tool4.activate();
-      view.onMouseMove = function (event) {};
+      myCircle3.strokeColor = "black";
+      myCircle3.fillColor = "white";
+      myCircle4.strokeColor = "black";
+      myCircle4.fillColor = "black";
+
+      view.onMouseMove = function (event) {
+        clientXPosition = event.point.x;
+        clientYPosition = event.point.y;
+      };
+
+      view.onFrame = function (event) {
+        if (!isNaN(clientXPosition) && !isNaN(clientYPosition)) {
+          xVector = clientXPosition - myCircle.position.x;
+          yVector = clientYPosition - myCircle.position.y;
+          vectorLength = Math.sqrt(xVector * xVector + yVector * yVector);
+          xVector /= vectorLength;
+          yVector /= vectorLength;
+
+          if (
+            pupilXDist ==
+            myCircle2.position.x - myCircle.position.x + xVector
+          ) {
+            pupilXDist = myCircle2.position.x - myCircle.position.x + xVector;
+          } else {
+            pupilXDist = myCircle2.position.x - myCircle.position.x + xVector;
+          }
+
+          if (
+            pupilYDist ==
+            myCircle2.position.y - myCircle.position.y + yVector
+          ) {
+          } else {
+            pupilYDist = myCircle2.position.y - myCircle.position.y + yVector;
+          }
+
+          pupilDist = Math.sqrt(
+            pupilXDist * pupilXDist + pupilYDist * pupilYDist
+          );
+
+          if (pupilDist < 10) {
+            myCircle2.position.x += xVector;
+            myCircle2.position.y += yVector;
+          }
+
+          pupilXDist2 = myCircle4.position.x - myCircle3.position.x + xVector;
+          pupilYDist2 = myCircle4.position.y - myCircle3.position.y + yVector;
+          pupilDist2 = Math.sqrt(
+            pupilXDist2 * pupilXDist2 + pupilYDist2 * pupilYDist2
+          );
+
+          if (pupilDist2 < 10) {
+            myCircle4.position.x += xVector;
+            myCircle4.position.y += yVector;
+          }
+
+          // Each frame, change the fill color of the path slightly by
+          // adding 1 to its hue:
+          path.fillColor.hue += 1;
+          let i = 0;
+          let randomValue;
+          addSubtract = !addSubtract;
+          do {
+            var sinus = Math.sin(event.time * 3 + i) * 3;
+            randomValue = Math.random() * 1;
+            var segment = path.segments[i];
+            var refSegment = copy.segments[i];
+            segment.point.y = refSegment.point.y + sinus;
+            segment.point.x = refSegment.point.x + sinus;
+            i++;
+          } while (!path.segments[i].isLast());
+        }
+      };
+
+      mouth.activate();
     }
   };
 
-  tool4 = new Tool();
-  tool4.onMouseDown = function (event) {
+  mouth = new Tool();
+  mouth.onMouseDown = function (event) {
     if (path.contains(event.point)) {
       var myCircle = new Path.Circle({
         center: event.point,
@@ -111,23 +243,3 @@ window.onload = function () {
 
   tool5 = new Tool();
 };
-
-// // Only executed our code once the DOM is ready.
-// window.onload = function () {
-//   // Get a reference to the canvas object
-//   var canvas = document.getElementById("myCanvas");
-//   // Create an empty project and a view for the canvas:
-//   paper.setup(canvas);
-//   // Create a Paper.js Path to draw a line into it:
-//   var path = new paper.Path();
-//   // Give the stroke a color
-//   path.strokeColor = "black";
-//   var start = new paper.Point(100, 100);
-//   // Move to start and draw a line from there
-//   path.moveTo(start);
-//   // Note that the plus operator on Point objects does not work
-//   // in JavaScript. Instead, we need to call the add() function:
-//   path.lineTo(start.add([200, -50]));
-//   // Draw the view now:
-//   paper.view.draw();
-// }
